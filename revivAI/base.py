f79cbd0d12ef3b7f99bfc14d2ae0149059bc86d6
fbaraket <fatma.baraket@mews-labs.com>
2025-01-29 11:14:13 +0100

    delete main in base

--- revivAI/base.py
+++ revivAI/base.py
@@ -120,63 +120,7 @@ class SurrogateModel:
                         raise ValueError(f'You must install the package of {module_name}')
                 print(f"Data is loaded from {file_h5}.")
 
-if __name__ == '__main__':
-    from catboost import CatBoostRegressor
 
-    model = CatBoostRegressor(n_estimators=100, max_depth=5, random_state=42)
-    # Initialisation du modèle et des données
-    surrogate_model = SurrogateModel()
-
-    # Données d'entraînement cohérentes
-    X = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]])
-    y = np.array([3, 6, 9, 12, 15])
-
-    surrogate_model.set(X, y, model)
-    surrogate_model.train()
-
-    # Prédiction sur de nouvelles données
-    X_new = np.array([[2, 3, 4], [5, 6, 7]])
-    surrogate_model.prediction(X_new)
-
-    # Sauvegarde des données
-    surrogate_model.dump("./model_data")
-    #from sklearn.ensemble import RandomForestRegressor
-    from catboost import CatBoostRegressor
-    #from sklearn.tree import DecisionTreeClassifier
-    # Créer une instance de modèle (par exemple, RandomForestRegressor)
-
-
-    #model = DecisionTreeClassifier()
-    surrogate_model = SurrogateModel()
-    X =[[0.76431152 ,0.66447784 ,0.40252745],
-        [0.77784719 ,0.2680739 , 0.51301999],
-        [0.37428748, 0.36702566 ,0.8352832 ],
-        [0.84944394, 0.52850647 ,0.28959054],
-        [0.04679972 ,0.24170513, 0.75792477]]
-    y = [0.3678914 , 0.3134304,  0.53511834 ,0.95278193, 0.04955847]
-
-    surrogate_model.set(X, y, model)
-    surrogate_model.train()
-    X_new =[[0.81176762 ,0.60318144, 0.94496069],
-             [0.64052046, 0.42480091 ,0.59984412],
-             [0.18545746, 0.6309216,  0.48833392],
-             [0.32811995 ,0.72646501, 0.41143818],
-             [0.51717996 ,0.85007028, 0.16759747]]
-
-    surrogate_model.prediction(X_new)
-
-    # Sauvegarder dans un dossier
-    surrogate_model.dump("./model_data")
-    test =[0.45523601 ,0.39392024 ,0.37198506 ,0.40304345 ,0.48881252]
-    # Charger le modèle et les données depuis le dossier
-
-    loaded_model = SurrogateModel()
-    loaded_model.load("./model_data")
-    # Prédire avec le modèle chargé
-    X_new = np.random.rand(5, 10)
-    loaded_model.train()
-    loaded_model.prediction(X_new)
-    print("Prédictions :", loaded_model.predict)
 
 
 
