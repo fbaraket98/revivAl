15e5a5324711615504c10b6d92d3ee11cf2f799a
ecieren <emmanuel.cieren@mews-labs.com>
2025-04-17 15:51:55 +0200

    added temp files to tests

--- test/test_base.py
+++ test/test_base.py
@@ -1,77 +1,62 @@
+import tempfile
+
 import numpy as np
+import pytest
+from catboost import CatBoostRegressor
 
 from revivai.base import LiteModel
 
 
-def test_X():
-    from catboost import CatBoostRegressor
-
-    model = CatBoostRegressor(n_estimators=100, max_depth=5, random_state=42)
+@pytest.fixture
+def lite_model():
+    model = CatBoostRegressor(
+        n_estimators=100,
+        max_depth=5,
+        random_state=42,
+        allow_writing_files=False,
+        silent=True,
+    )
     lite_model = LiteModel()
 
     X = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]])
     y = np.array([3, 6, 9, 12, 15])
+
     lite_model.set(X, y, model)
     lite_model.train()
 
     X_new = np.array([[2, 3, 4], [5, 6, 7]])
-
     lite_model.prediction(X_new)
-    path = "./metamodel_data"
-    lite_model.dump(path, "cat_boost")
 
-    load_model = LiteModel()
-    load_model.load(path, "cat_boost.h5")
+    return lite_model
+
+
+def test_X(lite_model):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        lite_model.dump(tmpdir, "cat_boost")
+        load_model = LiteModel()
+        load_model.load(tmpdir, "cat_boost.h5")
 
     assert np.allclose(
         load_model.X_train, lite_model.X_train
     ), "X_train data are not matching."
 
 
-def test_y():
-    from catboost import CatBoostRegressor
-
-    model = CatBoostRegressor(n_estimators=100, max_depth=5, random_state=42)
-    lite_model = LiteModel()
-
-    X = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]])
-    y = np.array([3, 6, 9, 12, 15])
-    lite_model.set(X, y, model)
-    lite_model.train()
-
-    X_new = np.array([[2, 3, 4], [5, 6, 7]])
-
-    lite_model.prediction(X_new)
-    path = "./metamodel_data"
-    lite_model.dump(path, "cat_boost")
-
-    load_model = LiteModel()
-    load_model.load(path, "cat_boost.h5")
+def test_y(lite_model):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        lite_model.dump(tmpdir, "cat_boost")
+        load_model = LiteModel()
+        load_model.load(tmpdir, "cat_boost.h5")
 
     assert np.allclose(
         load_model.y_train, lite_model.y_train
     ), "y_train data are not matching."
 
 
-def test_prediction():
-    from catboost import CatBoostRegressor
-
-    model = CatBoostRegressor(n_estimators=100, max_depth=5, random_state=42)
-    lite_model = LiteModel()
-
-    X = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]])
-    y = np.array([3, 6, 9, 12, 15])
-    lite_model.set(X, y, model)
-    lite_model.train()
-
-    X_new = np.array([[2, 3, 4], [5, 6, 7]])
-
-    lite_model.prediction(X_new)
-    path = "./metamodel_data"
-    lite_model.dump(path, "cat_boost")
-
-    load_model = LiteModel()
-    load_model.load(path, "cat_boost.h5")
+def test_prediction(lite_model):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        lite_model.dump(tmpdir, "cat_boost")
+        load_model = LiteModel()
+        load_model.load(tmpdir, "cat_boost.h5")
 
     assert np.allclose(
         load_model.predict, lite_model.predict
@@ -79,12 +64,17 @@ def test_prediction():
 
 
 def test_save_multi_output_model():
-    from catboost import CatBoostRegressor
     from sklearn import multioutput
     import pandas as pd
 
     model = multioutput.MultiOutputRegressor(
-        CatBoostRegressor(n_estimators=100, max_depth=5, random_state=42)
+        CatBoostRegressor(
+            n_estimators=100,
+            max_depth=5,
+            random_state=42,
+            allow_writing_files=False,
+            silent=True,
+        )
     )
     surrogate_model = LiteModel()
     X = pd.DataFrame(
@@ -100,10 +90,12 @@ def test_save_multi_output_model():
             "40%": [0.2453, 0.3456, 0.3654, 0.1234],
         }
     )
+
     surrogate_model.set(X, y, model)
     surrogate_model.train()
     surrogate_model.prediction(X)
-    path = "./metamodel_data"
-    surrogate_model.dump(path, "file_test")
-    load_model = LiteModel()
-    load_model.load(path, "file_test.h5")
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        surrogate_model.dump(tmpdir, "file_test")
+        load_model = LiteModel()
+        load_model.load(tmpdir, "file_test.h5")
